4a1341beb717c9bb278e85fe6297623830c610df
delairi <adriansw20@hotmail.com>
2025-07-30 21:32:41 -0500

    feat: add header

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,6 +1,9 @@
 const Header = () => {
   return (
-    <div className="h-1/12"></div>
+      <div className="container-row-between p-4 bg-[#f7f7f7]">
+        <h1 className="text-lg tracking-tighter">Delairi's photos</h1>
+        <button className="button">Login</button>
+      </div>
   )
 }
 
