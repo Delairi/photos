04d4eae11f2e1bee10c6d56333312de8ea5f9939
delairi <adriansw20@hotmail.com>
2025-10-02 01:11:49 -0500

    feat: setup s3

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -25,6 +25,13 @@ export type AmplifyDependentResourcesAttributes = {
       "Name": "string",
       "Region": "string"
     },
+    "S3Triggerf72c2f46": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "Signup": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
@@ -47,6 +54,12 @@ export type AmplifyDependentResourcesAttributes = {
       "LambdaExecutionRole": "string",
       "LambdaExecutionRoleArn": "string",
       "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "photos": {
+      "BucketName": "string",
       "Region": "string"
     }
   }
